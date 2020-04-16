2176fa87c358bb51ff2a224a0513e22e0aa45221
annaabbott <annaabbottdesign@gmail.com>
2020-04-15 21:06:32 -0600

    reformatting scripts

--- courses/2740/Hotel/scripts/scripts.js
+++ courses/2740/Hotel/scripts/scripts.js
@@ -1,38 +1,12 @@
 const apiURL = "../Hotel/hoteldata.json";            
 
-
-
-function createPhoneNumber(item) {
-    let phoneNumberDiv = document.createElement("div");
-    phoneNumberDiv.classList.add("infoColumnText");
-
-    let phoneNumberP = document.createElement("p");
-    phoneNumberP.innerText = item.phone;
-    phoneNumberDiv.appendChild(phoneNumberP);
-
-    return phoneNumberDiv;
-}
-
-function createInfoColumn(iconName) {
-    let iconDiv = document.createElement("div");
-    iconDiv.classList.add("infoColumnWrapper");
-
-    let iconTag = document.createElement("ion-icon");
-    iconTag.name= iconName;
-    iconDiv.appendChild(iconTag);
-    
-    iconDiv.appendChild(iconTag);
-    return iconDiv;
-}
-
-
-
 function buildPage(myList) {
     console.log(myList);
     let inventoryDiv = document.getElementById('inventoryWrapper');
     
-    for (item = 0; item < myList.length; item++) {
+    for (item = 0; item < myList.length; item++) { 
         let sectionTag = document.createElement("section");
+        // end section, one hotel card
 
         // creates figure in memory
         let imgTag = document.createElement("img");
@@ -42,16 +16,22 @@ function buildPage(myList) {
         figcaptionTag.textContent= myList[item].name;
     
         let figureTag = document.createElement("figure");
+        //appends img, figcaption in DOM
         figureTag.appendChild(imgTag);
         figureTag.appendChild(figcaptionTag);
         sectionTag.appendChild(figureTag);
-
+        //creates div.hotelinfo in memory
         let contactInfo = document.createElement("div");
         contactInfo.classList.add("hotelInfo");
-    
-        let leftColumn = createInfoColumn("car");
-
-        //create address
+        //creates div.infoColumnWrapper in memory
+        let leftColumn = document.createElement("div");
+        leftColumn.classList.add("infoColumnWrapper");
+        //creates car icon in memory, attaches to leftColumn in DOM
+        let iconTag = document.createElement("ion-icon");
+        iconTag.name= "car";
+        leftColumn.appendChild(iconTag);
+        
+        //create address (div.infoColumnText), adds to DOM
         let addressDiv = document.createElement("div");
         addressDiv.classList.add("infoColumnText");
     
@@ -67,19 +47,31 @@ function buildPage(myList) {
         addressLine.innerText = myList[item].address[2];
     
         addressDiv.appendChild(addressLine);
-
         leftColumn.appendChild(addressDiv);
-
-        ///------------------
-    
         contactInfo.appendChild(leftColumn);
+            //creates call icon in memory, attaches to rightColumn in DOM
+
+        let rightColumn = document.createElement("div");
+        rightColumn.classList.add("infoColumnWrapper");
     
-        let rightColumn = createInfoColumn("call");
-        rightColumn.appendChild(createPhoneNumber(myList[item]));
+        iconTag = document.createElement("ion-icon");
+        iconTag.name= "call";
+        rightColumn.appendChild(iconTag);
+        
+        rightColumn.appendChild(iconTag);
     
+        //create phone number
+        let phoneNumberDiv = document.createElement("div");
+        phoneNumberDiv.classList.add("infoColumnText");
+    
+        let phoneNumberP = document.createElement("p");
+        phoneNumberP.innerText = myList[item].phone;
+        phoneNumberDiv.appendChild(phoneNumberP);
+    
+        rightColumn.appendChild(phoneNumberDiv);
+  
         contactInfo.appendChild(rightColumn);
     
-
         sectionTag.appendChild(contactInfo);
 
         inventoryDiv.appendChild(sectionTag);
@@ -91,5 +83,5 @@ function main() {
         .then((response) => response.json())
         .then(data => buildPage(data));
 }
-
+//build the page.
 main();
